a07e102e71541ad9131efdca312ebf36326ebe18
harsh8055 <harshsingh.eth@gmail.com>
2024-10-22 14:52:22 +0530

    updated logs

--- src/coordinator.ts
+++ src/coordinator.ts
@@ -40,8 +40,8 @@ export function handleSubscriptionCreated(
   let consumer = RitualConsumer.bind(consumerAddress);
   let nameOfContainer = coordinator
     .getSubscription(event.params.id).containerId;
-  log.info("nameOfContainer", [nameOfContainer.toString()]);
-  if (nameOfContainer == Bytes.fromUTF8("qiro-policy")) {
+  log.info("nameOfContainer {}", [nameOfContainer.toString()]);
+    if (nameOfContainer == Bytes.fromUTF8("qiro-policy")) {
     let entity = new ComputeSubscription(cID);
     entity.blockTimestamp = event.block.timestamp;
     entity.transactionHash = event.transaction.hash;
@@ -90,7 +90,7 @@ export function handleSubscriptionFulfilled(
   let nameOfContainer = coordinator
     .getSubscription(event.params.id)
     .containerId;
-  log.info("nameOfContainer", [nameOfContainer.toString()]);
+  log.info("nameOfContainer {}", [nameOfContainer.toString()]);
   if (nameOfContainer == Bytes.fromUTF8("qiro-policy")) {
         // Load the subscription
     let sub = ComputeSubscription.load(cID);
