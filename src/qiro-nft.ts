f5e016fd6e403e8d55d77a5103f6fcfa923656a9
Rahul Gupta <rahul7668gupta@gmail.com>
2025-07-01 16:52:55 +0530

    removed more

--- src/qiro-nft.ts
+++ src/qiro-nft.ts
@@ -11,8 +11,6 @@ import {
   QiroNft,
 } from "../generated/QiroNFT/QiroNFT"
 import {
-  Approval,
-  ApprovalForAll,
   ConsumerContractUpdated,
   Deny,
   File,
@@ -21,36 +19,6 @@ import {
   Transfer,
   UpdateNftData,
 } from "../generated/schema"
-
-export function handleApproval(event: ApprovalEvent): void {
-  let entity = new Approval(
-    event.transaction.hash.concatI32(event.logIndex.toI32()),
-  )
-  entity.owner = event.params.owner
-  entity.approved = event.params.approved
-  entity.tokenId = event.params.tokenId
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleApprovalForAll(event: ApprovalForAllEvent): void {
-  let entity = new ApprovalForAll(
-    event.transaction.hash.concatI32(event.logIndex.toI32()),
-  )
-  entity.owner = event.params.owner
-  entity.operator = event.params.operator
-  entity.approved = event.params.approved
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
 
 export function handleConsumerContractUpdated(
   event: ConsumerContractUpdatedEvent,
