492f23df7fc7883f816b8f55c2271666c4c7d9ef
dharshan-qiro <dharshan@qiro.fi>
2025-12-10 15:15:55 +0530

    add transferRestrictions to metadata

--- src/qiro-nft.ts
+++ src/qiro-nft.ts
@@ -211,6 +211,7 @@ export function handleControlLayerMetadataSet(
   metadata.controlLogicHash = metadataStruct.getControlLogicHash();
   metadata.governanceKeysOrRoleMap =
     metadataStruct.getGovernanceKeysOrRoleMap();
+  metadata.transferRestrictions = metadataStruct.getTransferRestrictions();
   metadata.save();
 }
 
